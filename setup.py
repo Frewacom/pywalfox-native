fe2811b6a23db5c3f6235a375d52310ebcf91cb6
Fredrik Engstrand <frewacom@gmail.com>
2020-04-02 21:31:44 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import sys
+import setuptools
+
+import pywalfox.config
+
+LONG_DESC=open('README.md').read()
+VERSION=pywalfox.config.DAEMON_VERSION
+DOWNLOAD = "https://github.com/Frewacom/pywalfox-native/archive/%s.tar.gz" % VERSION
+
+setuptools.setup(
+    name="pywalfox",
+    version=VERSION,
+    author="Fredrik Engstrand",
+    author_email="fredrik@engstrand.nu",
+    description="Native app used alongside the Pywalfox browser extension",
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESC,
+    keywords="dynamic browser-theme firefox chrome duckduckgo unixporn native-messaging-host pywal colorscheme",
+    license="MIT",
+    url="https://github.com/Frewacom/pywalfox",
+    download_url=DOWNLOAD,
+    classifiers=[
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities"
+    ],
+    packages=["pywalfox"],
+    entry_points={"console_scripts": ["pywalfox=pywalfox.__main__:main"]},
+    python_requires=">=2.7",
+    include_package_data=True,
+    zip_safe=False)
