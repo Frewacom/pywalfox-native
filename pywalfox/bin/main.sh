fde86401db02c8d3cae9b45aa2128bdb4736e285
Fadde <obeyfranso@gmail.com>
2021-05-25 10:06:51 +0200

    feat: extend PATH variable if executing on macOS (#3)

--- pywalfox/bin/main.sh
+++ pywalfox/bin/main.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-python -m pywalfox start || python3 -m pywalfox start || python2.7 -m pywalfox start
+# Checks OS Version Number
+macOS=$(sw_vers -productVersion)
+if [[ ${#macOS} > 0  ]]; then
+    PATH="$PATH:/usr/local/bin"
+    pywalfox start
+fi
+
+python -m pywalfox start || python3 -m pywalfox start || python2.7 -m pywalfox start || python3.9 -m pywalfox start
